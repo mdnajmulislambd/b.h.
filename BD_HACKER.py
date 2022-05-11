dec43abe2e289cc521dba695c237f0c885295bc8
mdnajmulislambd <96902651+mdnajmulislambd@users.noreply.github.com>
2022-05-11 23:07:14 +0600

    Add files via upload

---
+++ BD_HACKER.py
@@ -0,0 +1,8 @@
+a ='\n\n\n\n BBBBBBBB	HH    HH'
+a+='\n BB 	BB	HH    HH'
+a+='\n BB     BB	HH    HH'
+a+='\n BBBBBBBB	HHHHHHHH'
+a+='\n BB     BB	HH    HH'
+a+='\n BB     BB °° 	HH    HH °°'
+a+='\n BBBBBBBB  °° 	HH    HH °°\n\n\n\n\n'
+print(a)
